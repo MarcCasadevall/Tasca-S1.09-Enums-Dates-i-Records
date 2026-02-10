116203a2dee9092e55aba2e7cf401617631883a2
MarcCasadevall <apple5.92@hotmail.com>
2026-02-10 16:16:18 +0100

    end ex1

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static enum Day {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
+    public static void checkDay(Day day) {
+        switch (day) {
+            case SATURDAY, SUNDAY -> System.out.println(day + " is weekend");
+            default -> System.out.println(day + " is a working day");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkDay(Day.MONDAY);
+        checkDay(Day.SATURDAY);
+    }
+
+}
